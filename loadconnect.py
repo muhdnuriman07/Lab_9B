614f1ea53818aa49b4602c0fe59ddf5201189e42
muhdnuriman07 <Cronaldo07@>
2020-06-22 16:30:00 +0800

    Lab9b_Iman

---
+++ loadconnect.py
@@ -0,0 +1,25 @@
+# First import the socket and sys library
+import socket;
+import sys;
+
+try:
+    #Where AF INET specify IPv4
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    print("Socket created successfully");
+except socket.error as err:
+    print("Socket creation failed with error %s"%(err));
+
+# Default web port
+
+port = 80;
+
+try:
+    host_ip = socket.gethostbyname("www.google.com");
+except socket.gaierror:
+    print("There is a problem resolving the host");
+    sys.exit();
+
+# Connect to the web server
+
+s.connect((host_ip,port));
+print("Successfully connected to the website given %s:%s"(host_ip, port));
